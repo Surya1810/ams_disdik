4400495208a237ed6dc1e7afea4035bab71c02a9
suhaefifzn <suhaefi21@gmail.com>
2025-05-10 11:36:53 +0700

    update: add route name to post /tag

--- routes/web/tag.php
+++ routes/web/tag.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TagController;
 Route::controller(TagController::class)->group(function () {
     Route::resource('tag', TagController::class)
         ->except(['store']);
-    Route::post('/tag', 'store')->middleware('auth.only.admin');
+    Route::post('/tag', 'store')->middleware('auth.only.admin')->name('tag.store');
     Route::get('/export/tag', [TagController::class, 'export'])->name('tag.export');
     Route::post('/tag/distribute', [TagController::class, 'distribute'])
         ->middleware('auth.only.operator')
