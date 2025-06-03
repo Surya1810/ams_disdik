cc6c1195d47a1d60d2132a99a1c60766cfc290f2
suhaefifzn <suhaefi21@gmail.com>
2025-06-03 17:15:09 +0700

    update: change image to Image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
                 aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
-                <img src="{{ asset('assets/image/logo/disdik_logo.png') }}" class="navbar-brand-img" alt="main_logo">
+                <img src="{{ asset('assets/Image/logo/disdik_logo.png') }}" class="navbar-brand-img" alt="main_logo">
             </a>
         </div>
         <hr class="horizontal dark my-0">
