3ad1c9b0e00cae9b58271b91e30005644e4a7933
suhaefifzn <suhaefi21@gmail.com>
2025-07-04 23:12:48 +0700

    update: delete max height in main tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -212,7 +212,7 @@
             </div>
         </div>
     </aside>
-    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+    <main class="main-content position-relative border-radius-lg">
         <!-- Navbar -->
         <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl text-white"
             id="navbarBlur" data-scroll="true">
